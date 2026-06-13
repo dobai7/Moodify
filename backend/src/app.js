927288f2f1b39ef1d64188145feb22347e348e53
Pratyush <dobaipratyush174@gmail.com>
2026-06-13 17:35:43 +0530

    Fix render port

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,12 @@ import cors from "cors"
 import songRouter from './routes/song.route.js';
 import stateRouter from './routes/state.route.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(cors({
   origin: "http://localhost:5173",
@@ -12,12 +18,14 @@ app.use(cors({
 }))
 
 app.use(express.json());
-app.use(express.static("../public"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/song",songRouter)
 app.use("/api/state",stateRouter)
 
-
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
 export default app;
